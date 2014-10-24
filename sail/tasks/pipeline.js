1da0a9f04c67dbca6b47757449f0fdd76b932a93
Don Hsieh <don@kindminds.com>
2014-10-24 23:21:24 +0800

    Add sail/assets/css/bootstrap.css

--- sail/tasks/pipeline.js
+++ sail/tasks/pipeline.js
@@ -24,14 +24,14 @@ var cssFilesToInject = [
 var jsFilesToInject = [
   
   // Load sails.io before everything else
-  'js/dependencies/sails.io.js',
+  //'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  //'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  //'js/**/*.js'
 ];
 
 
